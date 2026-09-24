821dcda6d50cb136f70a8f923461d7d6f0762cdd
wwliu1 <wwliu@terpmail.umd.edu>
2026-09-24 14:51:58 -0400

    Added new route for about.html page.

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def hello_world():  # put application's code here
 def hello():
     return render_template('hello.html')
 
+@app.route('/about')
+def abouto():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run()
